0efdc93ac8c3b32e09c336651360d9f88f87dbc1
Rob QIAO <rob.qiao@health.nsw.gov.au>
2023-10-23 20:37:03 +1100

    np.v0.9.1 - sschecker update sync

--- content/tso500samplesheetchecker/constants.py
+++
@@ -1,9 +0,0 @@
-"""
-Constants for use by module
-"""
-MSI_FIELDS = ["Usable MSI Sites",
-        "Total MSI Sites Unstable",
-        "Percent Unstable MSI Sites"]
-TMB_FIELDS = ["Total TMB",
-        "Coding Region Size in Megabases",
-        "Number of Passing Eligible Variants"]
